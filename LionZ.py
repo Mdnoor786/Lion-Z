bb265b14563c01c42e4e9c310ef1a58d4e11a40b
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-07-29 14:10:05 +0530

    Add files via upload

---
+++ LionZ.py
@@ -0,0 +1,6 @@
+from os import system
+system("git clone https://gitHub.com/mdnoor786/Lion-Z && cd Lion-Z && python3 -m userbot")
+# if got error
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LionXsupport")
+# Script written for stop heroku suspension
+# use with credit else gay
