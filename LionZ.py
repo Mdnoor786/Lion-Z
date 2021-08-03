a4d31caa192076ade6a20367ccc0de2f891ec227
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-08-03 16:46:12 +0530

    Update LionZ.py

--- LionZ.py
+++ LionZ.py
@@ -1,5 +1,8 @@
+import re, os, asyncio, html, logging
 from os import system
-
+#Logs
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.INFO)
+#Starting bot
 system(
     "git clone https://gitHub.com/mdnoor786/Lion-Z && cd Lion-Z && python3 -m userbot"
 )
