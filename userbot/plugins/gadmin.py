3e0448126a9c274e3cb9508beac78721d291af3f
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-08-06 21:13:27 +0530

    Update gadmin.py

--- userbot/plugins/gadmin.py
+++ userbot/plugins/gadmin.py
@@ -434,3 +434,173 @@ async def liongkick(event):  # sourcery no-metrics
             )
         if reply:
             await reply.forward_to(BOTLOG_CHATID)
+
+async def get_full_user(event):
+    args = event.pattern_match.group(1).split(":", 1)
+    extra = None
+    if event.reply_to_msg_id and not len(args) == 2:
+        previous_message = await event.get_reply_message()
+        user_obj = await event.client.get_entity(previous_message.sender_id)
+        extra = event.pattern_match.group(1)
+    elif len(args[0]) > 0:
+        user = args[0]
+        if len(args) == 2:
+            extra = args[1]
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            await event.edit("`Itz not possible without an user ID`")
+            return
+        if event.message.entities is not None:
+            probable_user_mention_entity = event.message.entities[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                user_obj = await event.client.get_entity(user_id)
+                return user_obj
+        try:
+            user_obj = await event.client.get_entity(user)
+        except Exception as err:
+            return await event.edit("Error... Please report at @LionxSupport", str(err))
+    return user_obj, extra
+
+
+global hawk, moth
+hawk = "admin"
+moth = "owner"
+
+
+async def get_user_from_id(user, event):
+    if isinstance(user, str):
+        user = int(user)
+    try:
+        user_obj = await event.client.get_entity(user)
+    except (TypeError, ValueError) as err:
+        await event.edit(str(err))
+        return None
+    return user_obj
+
+
+@lionub.lion_cmd(
+    pattern="gpromote ?(.*)",
+    command=("gpromote", plugin_category),
+    info={
+        "header": "To promote an user in every group where you are admin.",
+        "description": "Will promote the person in every group where you are admin only.",
+        "usage": "{tr}gpromote <username/reply/userid> <reason (optional)>",
+    },
+)
+async def gben(userbot):
+    dc = dark = userbot
+    i = 0
+    await dc.get_sender()
+    me = await userbot.client.get_me()
+    await dark.edit("`promoting...`")
+    my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
+    f"@{me.username}" if me.username else my_mention
+    await userbot.get_chat()
+    if userbot.is_private:
+        user = userbot.chat
+        rank = userbot.pattern_match.group(1)
+    else:
+        userbot.chat.title
+    try:
+        user, rank = await get_full_user(userbot)
+    except:
+        pass
+    if me == user:
+        await dark.edit("U want to promote urself 😑😑 waao..")
+        return
+    try:
+        if not rank:
+            rank = "ㅤㅤ"
+    except:
+        return await dark.edit(f"**Something W3NT Wrong 🤔**")
+    if user:
+        telchanel = [
+            d.entity.id
+            for d in await userbot.client.get_dialogs()
+            if (d.is_group or d.is_channel)
+        ]
+        rgt = ChatAdminRights(
+            add_admins=False,
+            invite_users=True,
+            change_info=False,
+            ban_users=True,
+            delete_messages=True,
+            pin_messages=True,
+        )
+        for x in telchanel:
+            try:
+                await userbot.client(EditAdminRequest(x, user, rgt, rank))
+                i += 1
+                await dark.edit(f"**Promoted in Chats **: `{i}`")
+            except:
+                pass
+    else:
+        await dark.edit(f"**Reply to a user you dumbo !!**")
+    return await dark.edit(
+        f"**Globally promoted [{user.first_name}](tg://user?id={user.id})\n On Chats😏 : {i} **"
+    )
+
+
+@lionub.lion_cmd(
+    pattern="gdemote ?(.*)",
+    command=("gdemote", plugin_category),
+    info={
+        "header": "To demote user in every group where you are admin.",
+        "description": "Will demote the person in every group where you are admin only.",
+        "usage": "{tr}gdemote <username/reply/userid> <reason (optional)>",
+    },
+)
+async def gben(userbot):
+    dc = dark = userbot
+    i = 0
+    await dc.get_sender()
+    me = await userbot.client.get_me()
+    await dark.edit("`demoting...`")
+    my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
+    f"@{me.username}" if me.username else my_mention
+    await userbot.get_chat()
+    if userbot.is_private:
+        user = userbot.chat
+        rank = userbot.pattern_match.group(1)
+    else:
+        userbot.chat.title
+    try:
+        user, rank = await get_full_user(userbot)
+    except:
+        pass
+    if me == user:
+        await dark.edit("U want to demote urself 😑😑 waao..")
+        return
+    try:
+        if not rank:
+            rank = "ㅤㅤ"
+    except:
+        return await dark.edit(f"**Something W3NT Wrong 🤔**")
+    if user:
+        telchanel = [
+            d.entity.id
+            for d in await userbot.client.get_dialogs()
+            if (d.is_group or d.is_channel)
+        ]
+        rgt = ChatAdminRights(
+            add_admins=None,
+            invite_users=None,
+            change_info=None,
+            ban_users=None,
+            delete_messages=None,
+            pin_messages=None,
+        )
+        for x in telchanel:
+            try:
+                await userbot.client(EditAdminRequest(x, user, rgt, rank))
+                i += 1
+                await dark.edit(f"**Demoted in Chats **: `{i}`")
+            except:
+                pass
+    else:
+        await dark.edit(f"**Reply to a user you dumbo !!**")
+    return await dark.edit(
+        f"**Globally Demoted [{user.first_name}](tg://user?id={user.id})\n On Chats😏 : {i} **"
+    )
