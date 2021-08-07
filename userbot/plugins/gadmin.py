fe8c26f2daebcc9810de0e61048cc3d98740c163
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-08-07 18:44:59 +0530

    Update gadmin.py

--- userbot/plugins/gadmin.py
+++ userbot/plugins/gadmin.py
@@ -6,7 +6,17 @@ from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import ChatBannedRights
 from telethon.utils import get_display_name
-
+from telethon.errors.rpcerrorlist import (UserIdInvalidError,
+                                            MessageTooLongError)
+from telethon.tl.functions.channels import (EditAdminRequest,
+                                              EditBannedRequest,
+                                                EditPhotoRequest)
+from telethon.tl.functions.messages import UpdatePinnedMessageRequest
+from telethon.tl.types import (ChannelParticipantsAdmins,
+                                 ChatAdminRights,
+                                   ChatBannedRights,
+                                     MessageEntityMentionName,
+                                       MessageMediaPhoto)
 from userbot import lionub
 
 from ..funcs.managers import edit_delete, edit_or_reply
