1708a1c34ea1d1a2266b9263e2b3865d5ac33fa2
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-08-14 23:12:52 +0530

    Update stringsetup.py

--- stringsetup.py
+++ stringsetup.py
@@ -8,10 +8,20 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 print(
-    """Please go-to my.telegram.org
-Login using your Telegram account
-Click on API Development Tools
-Create a new application, by entering the required details"""
+    """𝙿𝙻𝙴𝙰𝚂𝙴 𝙶𝙾 𝚃𝙾 my.telegram.org
+𝙻𝙾𝙶𝙸𝙽 𝚄𝚂𝙸𝙽𝙶 𝚈𝙾𝚄𝚁 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼 𝙰𝙲𝙲𝙾𝚄𝙽𝚃
+𝙲𝙻𝙸𝙲𝙺 𝙾𝙽 𝙰𝙿𝙸 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙼𝙴𝙽𝚃 𝚃𝙾𝙾𝙻𝚂
+𝙲𝚁𝙴𝙰𝚃𝙴 𝙰 𝙽𝙴𝚆 𝙰𝙿𝙿𝙻𝙸𝙲𝙰𝚃𝙸𝙾𝙽, 𝙱𝚈 𝙴𝙽𝚃𝙴𝚁𝙸𝙽𝙶 𝚁𝙴𝚀𝚄𝙸𝚁𝙴𝙳 𝙳𝙴𝚃𝙰𝙸𝙻𝚂
+𝚃𝙴𝙰𝙼𝙻𝙸𝙾𝙽-Z
+ _       _____   ____   _   _ _
+| |     |_   _| / __ \\ | \\ | |
+| |       | |  | |  | | |  \\| |
+| |       | |  | |  | | | . \. |
+| |____  _| |_ | |__| | | |\\  |
+|______||_____| \\____/ |_| \\_|
+
+
+Running Lion Fire Z 🔥🔥🔥🔥...."""
 )
 APP_ID = int(input("Enter APP ID here: "))
 API_HASH = input("Enter API HASH here: ")
